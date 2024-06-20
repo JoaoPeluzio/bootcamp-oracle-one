e368cb358ba61edcdec02a81cffed71254402afa
JoaoPeluzio <jvpeluzio@gmail.com>
2024-06-20 17:31:48 -0300

    numero secreto game (incomplete)

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,1 +1,23 @@
 alert('Bem-vindo ao jogo do número secreto!');
+
+function jogoDoChute() {
+
+let numeroSecreto = 5;
+let stringVazia = '';
+
+let chute = prompt('Escolha um número entre 1 e 30', 'Digite aqui');
+
+let chutePergunta = Number.isInteger(chute);
+
+if (numeroSecreto === chutePergunta) {
+    alert('Você acertou!');
+} else if (numeroSecreto !== chutePergunta || chutePergunta == stringVazia) {
+    alert('Você colocou um número inválido ou uma letra ou espaço vazio'); 
+    jogoDoChute();  
+} else if (chutePergunta === null) {
+    alert('Você saiu!');
+};
+
+};
+
+jogoDoChute();
