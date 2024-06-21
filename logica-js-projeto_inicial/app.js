649d39612c6c13b75a18f4075027be65b72aeda0
JoaoPeluzio <jvpeluzio@gmail.com>
2024-06-21 15:55:10 -0300

    aula 1

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,23 +1,17 @@
+
 alert('Bem-vindo ao jogo do número secreto!');
 
-function jogoDoChute() {
 
 let numeroSecreto = 5;
 let stringVazia = '';
 
 let chute = prompt('Escolha um número entre 1 e 30', 'Digite aqui');
 
-let chutePergunta = Number.isInteger(chute);
 
-if (numeroSecreto === chutePergunta) {
-    alert('Você acertou!');
-} else if (numeroSecreto !== chutePergunta || chutePergunta == stringVazia) {
-    alert('Você colocou um número inválido ou uma letra ou espaço vazio'); 
-    jogoDoChute();  
-} else if (chutePergunta === null) {
-    alert('Você saiu!');
+if (numeroSecreto == chute) {
+    alert(`Você acertou! número secreto: ${numeroSecreto}`);
+} else {
+    alert('Você errou o número!')
 };
 
-};
 
-jogoDoChute();
---
+++ logica-js-projeto_inicial/desafios.js
@@ -0,0 +1,22 @@
+//Desafio de aula
+
+alert('Welcome to our page!');
+
+let name = 'Lua';
+let age = 25;
+let numberSellings = 50;
+let avaiableBalance = 1000;
+
+
+alert('Error! Fill out all the blank spaces.');
+
+let errorMessage = 'Error! Fill out all the blank spaces.';
+alert(errorMessage);
+
+let promptName = prompt(" What's your name?");
+
+let promptAge = prompt(" What's your age?");
+
+if (age >= 18) {
+    alert("You can get your driver's license!");
+};
