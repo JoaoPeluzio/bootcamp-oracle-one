b257027fe7575b8757754eab4397b432cff9cbd3
JoaoPeluzio <jvpeluzio@gmail.com>
2024-06-26 11:18:18 -0300

    aula 9 - arrays e desafio

---
+++ logica-js-aula_9/desafio.js
@@ -0,0 +1,23 @@
+//DESAFIOS
+
+
+//crie uma lista vazia;
+let array = [];
+
+//Crie uma lista de linguagens de programação chamada linguagensDeProgramacao com os seguintes elementos: 'JavaScript','C','C++', 'Kotlin' e 'Python'.
+
+let arrayLinguagens = ['javascript', 'C', 'C++', 'Kotlin', 'Python'];
+
+//Adicione à lista linguagensDeProgramacao os seguintes elementos: 'Java', 'Ruby' e 'GoLang'.
+
+arrayLinguagens.push('java', 'Ruby', 'GoLang');
+
+console.log(arrayLinguagens);
+
+//Crie uma lista com 3 nomes e exiba no console apenas o primeiro elemento.
+
+let arrayNomes = ['João', 'Maria', 'Thiago'];
+console.log(arrayNomes[0]); //loga o primeiro nome
+console.log(arrayNomes[1]); //loga o segundo nome
+console.log(arrayNomes[2]); //loga o terceiro nome
+
