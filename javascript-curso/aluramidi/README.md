a493df16bf9cf5de2f6a1ecdbc915a07052edfc2
JoaoPeluzio <jvpeluzio@gmail.com>
2024-10-13 18:43:43 -0300

    atualizando pastas e adicionando README

---
+++ javascript-curso/aluramidi/README.md
@@ -0,0 +1,22 @@
+<p align="center">
+  <img src="./github/aluramid.png" alt="Demonstração do projeto" width="100%" />
+</p>
+
+
+## 🖥️ Projeto
+
+O projeto é um estudo de Javascript para o curso Oracle ONE em parceria com a Alura, onde estudamos funções, listas e manipulação de elementos no DOM.
+
+## 🛠️ Funcionalidades
+
+O usuário poderá clicar nos botões para ouvir os áudios ou utilizar o tab no teclado para navegar pelos botões e pressionar enter ou espaço para ouvir os sons. 
+
+## 🚀 Tecnologias
+
+Esse projeto foi desenvolvido com as seguintes tecnologias:
+
+- HTML
+- CSS
+- JAVASCRIPT
+- GIT e GITHUB
+
--- javascript-curso/aluramidi/main.js
+++ javascript-curso/aluramidi/main.js
@@ -1,15 +1,30 @@
-function tocaSom (idElementoAudio) {
-  document.querySelector(idElementoAudio).play();
+function tocaSom(seletorAudio) {
+  const elemento = document.querySelector(seletorAudio);  
+
+  if (elemento !== null && elemento.localName === "audio") {
+      elemento.play();
+  } else {
+    console.error("O elemento deve ser um audio.");
+  }
 }
+const listaDeTeclas = document.querySelectorAll(".tecla");
 
-const listaDeTeclas = document.querySelectorAll('.tecla');
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+  const tecla = listaDeTeclas[contador];
+  const instrumento = tecla.classList[1];
+  const idAudio = `#som_${instrumento}`;
 
-let contador = 0;
+  tecla.onclick = function () {
+    tocaSom(idAudio);
+  };
 
-while(contador < listaDeTeclas.length) {
+  tecla.onkeydown = function (evento) {
+    if (evento.code === "Space" || evento.code === "Enter") {
+      tecla.classList.add("ativa");
+    }
+  };
 
-  listaDeTeclas[contador].onclick = tocaSom;
-
-  contador++;
-  
+  tecla.onkeyup = function () {
+    tecla.classList.remove("ativa");
+  };
 }
