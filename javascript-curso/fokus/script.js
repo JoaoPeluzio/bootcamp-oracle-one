42534bf136be7cd6fd7f42db3b365e705c77d6ce
JoaoPeluzio <jvpeluzio@gmail.com>
2024-10-21 20:15:16 -0300

    atualizacoes

--- javascript-curso/fokus/script.js
+++ javascript-curso/fokus/script.js
@@ -5,7 +5,26 @@ const longoBt = document.querySelector('.app__card-button--longo');
 const banner = document.querySelector('.app__image');
 const titulo = document.querySelector('.app__title');
 const botoes = document.querySelectorAll('.app__card-button'); //cria um array de lista de classes
+const startPauseBt = document.querySelector('#start-pause');
 
+const musicaFocoInput = document.querySelector('#alternar-musica');
+const musica = new Audio('/sons/luna-rise-part-one.mp3');
+const somPlay = new Audio('/sons/play.wav');
+const somPause = new Audio('/sons/pause.mp3');
+const somBeep = new Audio('/sons/beep.mp3');
+
+let tempoDecorridoEmSegundos = 5;
+let intervalodId = null;
+
+
+musica.loop = true;
+musicaFocoInput.addEventListener('change', () => {
+  if(musica.paused) {
+    musica.play();
+  } else {
+    musica.pause();
+  }
+});
 focoBt.addEventListener('click', () => {
   alterarContexto('foco');
   focoBt.classList.add('active');
@@ -47,7 +66,33 @@ function alterarContexto(contexto) {
       break;
   }
 
-
-  
 }
 
+const contagemRegressiva = () => {
+  if(tempoDecorridoEmSegundos <= 0) {
+    zerar();
+    console.log('Tempo esgotado!');
+    return;
+  }
+  tempoDecorridoEmSegundos -= 1;
+  console.log('Temporizador ' + tempoDecorridoEmSegundos);
+}
+
+startPauseBt.addEventListener('click', iniciarOuPausar);
+
+function iniciarOuPausar() {
+
+  //
+
+  if(intervalodId) {
+    zerar();
+    return;
+  }
+
+  intervalodId = setInterval(contagemRegressiva, 1000);
+};
+
+function zerar() {
+  clearInterval(intervalodId);
+  intervalodId = null;
+};
