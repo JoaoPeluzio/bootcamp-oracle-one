70fae18d5c4ad0d8d1926436bac528f8e238f252
JoaoPeluzio <jvpeluzio@gmail.com>
2024-10-28 17:47:54 -0300

    aula de awayt async functions

--- javascript-curso/alurabooks/script.js
+++ javascript-curso/alurabooks/script.js
@@ -1,4 +1,19 @@
-const pegaCep = fetch('https://viacep.com.br/ws/01001000/json/')
+async function buscaEndereco() {
+    
+    const consultaCEP = await fetch('https://viacep.com.br/ws/26383060/json/')
+
+    const consultaCepConvertida = await consultaCEP.json();
+
+    console.log(consultaCepConvertida);
+}
+
+buscaEndereco();
+
+
+//await faz toda a conversão para json;
+
+//Antigo código (sem async/await)
+/*
 .then(resposta => resposta.json())
 .then(r => {
 
@@ -12,6 +27,8 @@ const pegaCep = fetch('https://viacep.com.br/ws/01001000/json/')
 .finally(mensagem => console.log('Processamento concluído!'));
 
 console.log(pegaCep);
+*/
 
 //then ocorre quando a promessa retorna resolvida
 //catch ocorre quando a promessa é rejeitada 
+
