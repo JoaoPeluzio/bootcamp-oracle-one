76d7fc3c0ca2024347f4774241cce690904a4984
JoaoPeluzio <jvpeluzio@gmail.com>
2024-10-30 16:58:16 -0300

    aula de async/await

--- javascript-curso/alurabooks/script.js
+++ javascript-curso/alurabooks/script.js
@@ -1,29 +1,21 @@
-/*
+async function buscaEndereco(cep) {
+    
+    try {
+        const consultaCEP = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+    
+        const consultaCEPbinaryToJson = await consultaCEP.json(); //convertemos o binario que vem da API para json
 
---- Código da conversa versão síncrono ---
+        if(consultaCEPbinaryToJson.erro) {
+            throw new Error(`Não foi possível buscar o endereço. Código de status: ${consultaCEPbinaryToJson.status}`);
+        } //fazemos o tratamento do erro, caso nao consiga encontrar o que foi pedido.
+        
+        console.log('Endereço:', consultaCEPbinaryToJson);
 
-console.log("Mandando oi pro amigo!");
-
-function mandaMensagem() {
-    console.log("Tudo bem?");
-    console.log("Vou te mandar uma solicitação!");
-    console.log("Solicitação recebida!");
+        return consultaCEPbinaryToJson; //retorna o endereço caso tenha sido encontrado.
+    } catch (erro) {
+        console.log('Não foi possível buscar o endereço: ' + erro);
+        return;
+    }
 }
 
-mandaMensagem();
-
-console.log("Tchau tchau!");
-
-*/
-
-console.log("Mandando oi pro amigo!");
-
-function mandaMensagem() {
-    console.log("Tudo bem?");
-    console.log("Vou te mandar uma solicitação!");
-    console.log("Solicitação recebida!");
-}
-
-setTimeout(mandaMensagem, 5000);
-
-console.log("Tchau tchau!");
+buscaEndereco('26383060');
