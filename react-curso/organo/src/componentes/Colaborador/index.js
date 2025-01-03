b0ecb74068326335be59a44857122881c339eaea
JoaoPeluzio <jvpeluzio@gmail.com>
2025-01-02 22:58:05 -0300

    adicionando feature de favoritar

--- react-curso/organo/src/componentes/Colaborador/index.js
+++ react-curso/organo/src/componentes/Colaborador/index.js
@@ -1,10 +1,18 @@
-import { IoIosCloseCircle } from "react-icons/io";
+import { AiFillCloseCircle, AiFillHeart, AiOutlineHeart } from "react-icons/ai";
 import "./colaborador.css";
 
-const Colaborador = ({ colaborador, corDeFundo, aoDeletar }) => {
+const Colaborador = ({ colaborador, corDeFundo, aoDeletar, aoFavoritar }) => {
+  function favoritar() {
+    aoFavoritar(colaborador.id);
+  }
+
+  const propsFavorito = {
+    size: 25,
+    onClick: favoritar
+  }
   return (
     <div className="colaborador">
-      <IoIosCloseCircle 
+      <AiFillCloseCircle 
         size={25} 
         className="deletar" 
         onClick={() => aoDeletar(colaborador.id)} 
@@ -15,6 +23,12 @@ const Colaborador = ({ colaborador, corDeFundo, aoDeletar }) => {
       <div className="rodape">
         <h4>{colaborador.nome}</h4>
         <h5>{colaborador.cargo}</h5>
+        <div className="favoritar">
+          {colaborador.favorito 
+            ? <AiFillHeart {...propsFavorito} color='#ff0000'/> 
+            : <AiOutlineHeart {...propsFavorito} />
+          }
+        </div>
       </div>
     </div>
   );
