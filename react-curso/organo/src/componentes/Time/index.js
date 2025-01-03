9232f42970b0ac88f0e868e257a5c756e890a448
JoaoPeluzio <jvpeluzio@gmail.com>
2025-01-02 21:41:43 -0300

    criando ids para os usuarios

--- react-curso/organo/src/componentes/Time/index.js
+++ react-curso/organo/src/componentes/Time/index.js
@@ -1,20 +1,21 @@
 import Colaborador from '../Colaborador'
 import './time.css'
+import hexToRgba from 'hex-to-rgba';
 
 const Time = ({ time, colaboradores, aoDeletar, mudarCor }) => {
     return (
 
-        colaboradores.length > 0 && <section className='time' style={{ backgroundImage: 'url(/imagens/fundo.png)', backgroundColor: time.corPrimaria }}>
+        colaboradores.length > 0 && <section className='time' style={{ backgroundImage: 'url(/imagens/fundo.png)', backgroundColor: hexToRgba(time.cor, '0.6') }}>
             <input 
-                value={time.corSecundaria} 
-                onChange={evento => mudarCor(evento.target.value, time.nome)}   type='color' 
+                value={time.cor} 
+                onChange={evento => mudarCor(evento.target.value, time.id)}   type='color' 
                 className='input-cor'>
             </input>
 
-            <h3 style={{ borderColor: time.corSecundaria }}>{time.nome}</h3>
+            <h3 style={{ borderColor: time.cor }}>{time.nome}</h3>
             <div className='colaboradores'>
                 {colaboradores.map((colaborador, indice) => {
-                    return <Colaborador key={indice} colaborador={colaborador} corDeFundo={time.corSecundaria} aoDeletar={aoDeletar}/>
+                    return <Colaborador key={indice} colaborador={colaborador} corDeFundo={time.cor} aoDeletar={aoDeletar}/>
                 })}
             </div>
         </section>
